8516125903668ca92a77ddb2eceef8838946ebe2
Gregory Jefferis <jefferis@gmail.com>
2018-05-29 17:47:16 +0100

    doc polishing

--- R/data.R
+++ R/data.R
@@ -1,19 +1,19 @@
 #' Neuron list constructed from neuronlistfh with delayedAssign
 #'
 #' @details The source data for this object live in \code{extdata} and consist
-#'   of a metadata object saved as \code{{pnsnlfh.rds}} along with a \code{data}
+#'   of a metadata object saved as \code{pnsnlfh.rds} along with a \code{data}
 #'   folder. When the package is loaded, an in memory version of the data is
 #'   constructed using \code{\link{delayedAssign}}.
 #' @docType data
 #' @name pnsfromnlfh
-NULL
+"pnsfromnlfh"
 
 #' neuronlistfh object made with delayedAssign
 #'
 #' @details The source data for this object live in \code{extdata} and consist
-#'   of a metadata object saved as \code{{pnsnlfh.rds}} along with a \code{data}
+#'   of a metadata object saved as \code{pnsnlfh.rds} along with a \code{data}
 #'   folder. When the package is loaded the neuronlistfh object is loaded into
 #'   an object called \code{pnsnlfh} using \code{\link{delayedAssign}}.
 #' @docType data
 #' @name pnsnlfh
-NULL
+"pnsnlfh"
